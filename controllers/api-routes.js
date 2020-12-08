46c64644b1add30ec76c0fee3103580bf2fe95cc
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-12-07 21:12:37 -0500

    Fixed issue with schema, defined /api/wokouts route

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -3,8 +3,14 @@ const router = express.Router()
 
 const Workout = require('../models/workout-model')
 
-router.get('/api/workouts', function (req, res) {
-  res.json()
+router.get('/api/workouts', async function (req, res) {
+  const workouts = await Workout.find({})
+  console.log(workouts)
+  try {
+    res.status(200).json(workouts)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 router.get('/api/workouts/range', function (req, res) {
--- models/workout-model.js
+++ models/workout-model.js
@@ -5,16 +5,18 @@ const Schema = mongoose.Schema
 const workoutSchema = new Schema({
   day: {
     type: Date,
+    unique: true,
   },
   exercises: [
-    {
+    new Schema({
       type: String,
       name: String,
       duration: Number,
       weight: Number,
       rep: Number,
       sets: Number,
-    },
+      distance: Number,
+    }),
   ],
 })
 
