166359fafbbab93254e407f4b1bc6bb83fa73fe9
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-12-09 19:29:35 -0500

    Fixed the post and put routes to push a new workout to array instead of setting it

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -5,8 +5,8 @@ const Workout = require('../models/workout-model')
 
 // A route that finds all the workouts in the database.
 router.get('/api/workouts', async function (req, res) {
-  const workouts = await Workout.find({})
   try {
+    const workouts = await Workout.find({})
     res.status(200).json(workouts)
   } catch (err) {
     res.status(500).json(err)
@@ -14,38 +14,26 @@ router.get('/api/workouts', async function (req, res) {
 })
 
 // A route that creates a new workout in the database when the New Workout button is pressed.
-// The workout is created for the current day, and a new one will not be created if one already exists.
+// The workout is created for the current day.
 // The exercises in the workout are added through the put route.
 router.post('/api/workouts', async function (req, res) {
   try {
-    const newWorkout = await new Workout()
-    const existingWorkout = await Workout.findOne({ day: newWorkout.day })
+    const newWorkout = await Workout.create(req.body)
     // console.log(newWorkout)
-    // console.log(existingWorkout)
-    // Check database to see if workout already exists for current day. If not, create the new workout.
-    if (!existingWorkout) {
-      newWorkout.save()
-      res.status(201).json(newWorkout)
-    } else {
-      res.status(200).json(existingWorkout)
-    }
+    res.status(201).json(newWorkout)
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
 // A route that is triggered when the Add Exercises button is pressed when creating a new workout.
-// The workout created through the post route is updated with the field values from the form.
-// If a workout already exists for the current day, the previous workout is overwritten.
+// The new workout object is pushed into an array for the current day.
 router.put('/api/workouts/:id', async function (req, res) {
-  const id = req.params.id
-  const workoutToUpdate = await Workout.updateOne(
-    { _id: id },
-    { $set: { exercises: req.body } }
-  )
-  const updatedWorkout = await Workout.findOne({ _id: id })
   try {
-    res.status(200).json(updatedWorkout)
+    const workoutToUpdate = await Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    })
+    res.status(200).json(workoutToUpdate)
   } catch (err) {
     res.status(500).json(err)
   }
